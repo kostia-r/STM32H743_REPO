48101899d469a6bd6072abccba7dbfe6686563ad
kos <rudenko.x64@gmail.com>
2024-11-11 01:46:57 +0100

    Initial CubeMX + Keil Project for STM32H743VIT6.

---
+++ MDK-ARM/RTE/_STM32H743_REPO/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32H743_REPO' 
+ * Target:  'STM32H743_REPO' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
